e8e471e69f352a511280eef91aeaaa4c0db96776
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2018-04-08 12:37:50 +0200

    normalize matrix

--- matrix.py
+++ matrix.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 X = np.random.normal(loc=1, scale=10, size=(1000, 50))
+XcolMean=np.mean(X,axis=0)
+XcolStd=np.std(X,axis=0)
+print ('mean: ')
+print (XcolMean)
+print ('; std: ')
+print (XcolStd)
+
+X_norm = ((X-XcolMean)/XcolStd)
+print ("matrix")
 print (X)
+print ("matrix - mean")
+print (X-XcolMean)
+print ("(matrix - mean)/std")
+print (X_norm)
